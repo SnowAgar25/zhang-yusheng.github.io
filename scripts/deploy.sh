89f56a05412c5a93a754ad3d8d0ad6a42c41cf5c
Tsuyumi <SnowU1310828@gmail.com>
2024-10-17 18:32:11 +0800

    fix: test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,6 @@
    git commit -m 'deploy'
 
    # 如果發佈到 https://<USERNAME>.github.io/<REPO>
-   git push -f git@github.com:SnowAgar25/zhang-yusheng.github.io.git test/vitepress:gh-pages
+   git push -f git@github.com:SnowAgar25/zhang-yusheng.github.io.git main:gh-pages
 
    cd -
